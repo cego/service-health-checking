9eef859e2ea4f255ebd5891e7be6666395f8734d
Thomas Wogelius <thwo@cego.dk>
2021-10-25 15:30:28 +0200

    Return empty string

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@ use Illuminate\Support\Facades\Route;
 use Cego\ServiceHealthChecking\Controllers\ServiceHealthCheckingController;
 
 Route::get('/vendor/service-health-checking', [ServiceHealthCheckingController::class, 'index'])->name('vendor.service-health-checking.index');
-Route::get('/vendor/service-health-checking/ping', fn () => response())->name('vendor.service-health-checking.ping');
+Route::get('/vendor/service-health-checking/ping', fn () => '')->name('vendor.service-health-checking.ping');
